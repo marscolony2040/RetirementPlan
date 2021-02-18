1646af42359f9211814145879134b3f248b89e19
ElluminAI <mo.sharieff47@gmail.com>
2021-02-18 11:49:40 -0500

    update

--- retirement.py
+++ retirement.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
 def pv(amount, rate, maturity):
     return (amount / rate) * (1 - pow(1 + rate, -maturity))
 
@@ -14,12 +16,12 @@ def table(withdrawl=100000, save=5, withdraw=4, rate=0.03):
     ipmt = 0
     payment = 0
     hold = []
-    hold.append([balance, ipmt, payment])
+    hold.append([balance, ipmt, -payment])
     for i in range(save):
         payment = present_payment
         ipmt = balance * rate
         balance = balance + ipmt + payment
-        hold.append([balance, ipmt, payment])
+        hold.append([balance, ipmt, -payment])
     for i in range(withdraw):
         payment = withdrawl
         ipmt = balance * rate
@@ -27,4 +29,4 @@ def table(withdrawl=100000, save=5, withdraw=4, rate=0.03):
         hold.append([balance, ipmt, payment])
     return pd.DataFrame(data=hold, columns=['Balance','Interest','Pmt/Wth'])
 
-print(table(withdrawl=500, save=10, withdraw=5, rate=0.09))
+print(table(withdrawl=4500, save=3, withdraw=5, rate=0.09))
